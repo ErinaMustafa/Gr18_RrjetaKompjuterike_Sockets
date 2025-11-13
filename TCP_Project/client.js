b666f7f2e436e5f53cfc7e02bc370246834e19ad
Erisa Sollova <erisasollova@gmail.com>
2025-11-13 23:18:32 +0100

    Update

--- TCP_Project/client.js
+++ TCP_Project/client.js
@@ -12,3 +12,17 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+klient.connect(SERVER_PORT, SERVER_IP, () => {
+    console.log(' Lidhja me server u kry me sukses!');
+    console.log('Shkruaj "ADMIN" për të hyrë si admin (do të kërkohet fjalëkalimi), ose shkruaj mesazh normal:');
+});
+
+klient.on('data', (data) => {
+    console.log(` Nga serveri: ${data.toString()}`);
+});
+
+klient.on('close', () => {
+    console.log(' Lidhja me serverin u mbyll.');
+    process.exit(0);
+});
