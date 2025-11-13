19233a61d2efad7c3c38592779f35f8b8526d549
ErinaMustafa <erina.mustafa@student.uni-pr.edu>
2025-11-13 15:08:23 +0100

    Update server.js

--- TCP_Project/server.js
+++ TCP_Project/server.js
@@ -3,21 +3,21 @@ const fs = require('fs');
 const path = require('path');
 
 const PORT = 6000;
-const HOST = '0.0.0.0';
+const HOST = '0.0.0.0'; 
 const MAX_CONNECTIONS = 5;
-const USER_TIMEOUT = 30000;
-const ADMIN_TIMEOUT = 10000;
+const USER_TIMEOUT = 30000; 
+const ADMIN_TIMEOUT = 10000; 
+
 let connections = new Map();
 let totalTraffic = 0;
 let messageLog = [];
 const STATS_FILE = 'server_stats.txt';
 const SERVER_FILES = './server_files';
-
 if (!fs.existsSync(SERVER_FILES)) fs.mkdirSync(SERVER_FILES);
 
 const server = net.createServer((socket) => {
   if (connections.size >= MAX_CONNECTIONS) {
-    socket.write(' Server is full, try again later.\n');
+    socket.write('Server is full, try again later.\n');
     socket.end();
     return;
   }
