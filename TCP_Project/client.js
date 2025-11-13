a4e03bb9019324650e5edd075d2255521c63511c
Erisa Sollova <erisasollova@gmail.com>
2025-11-13 23:17:16 +0100

    vazhdim i client.js

--- TCP_Project/client.js
+++ TCP_Project/client.js
@@ -5,3 +5,10 @@ const readline = require('readline');
 const SERVER_IP = '127.0.0.1'; 
 const SERVER_PORT = 4000;
 
+
+const klient = new net.Socket();
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
